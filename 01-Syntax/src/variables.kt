6a18a1e45011062b8bddf74472ccaa3c2d7fdcdb
Roman Metelov <rm0734861369@gmail.com>
2020-12-02 16:01:01 +0200

    some changes

--- 01-Syntax/src/variables.kt
+++ 01-Syntax/src/variables.kt
@@ -1,3 +1,94 @@
+/*
+// Hello, Kotlin
+
 fun main() {
-    println("variable00001")
+    println("Hello, Kotlin")
 }
+*/
+
+
+
+/*
+// variables
+
+fun main() {
+    val a = 5
+    var b = 6
+    b = a
+    println(a + b)
+
+    val x: Int = 5
+    var y: Int? = null
+
+    y = 6
+
+    println(x.toString() + y?.toString())
+}
+ */
+
+
+
+/*
+// const
+
+const val a = "someConstant"
+
+fun main() {
+    println(a)
+}
+ */
+
+
+
+/*
+// whole numbers
+
+val one = 1 // Int
+val threeBillion = 3000000000 // Long
+val oneLong = 1L // Long
+val oneByte: Byte = 1
+
+// fractional numbers
+val pi = 3.14 // Double
+val e = 2.7182818284 // Double
+val eFloat = 2.7182818284f // Float, actual value is 2.7182817
+
+//Boolean
+val boolean = true
+val a = !boolean
+ */
+
+
+
+/*
+// Arrays
+
+// Array of int of size 5 with values [0, 0, 0, 0, 0]
+val arr = IntArray(5)
+
+// e.g. initialise the values in the array with a constant
+// Array of int of size 5 with values [42, 42, 42, 42, 42]
+val arr2 = IntArray(5) { 42 }
+
+fun main() {
+    val array = arrayOf(1, 2, 3)
+    array.forEach { println(it) }
+}
+ */
+
+
+
+/*
+// Strings
+// abc1def
+fun main() {
+    val s = "abc" + 1
+    println(s + "def)
+}
+
+// abc.length is 3
+fun main() {
+    val s = "abc"
+    println("$s.length is ${s.length}")
+}
+ */
